ba1a82ab70964fd3bb2a323d6fc5cf1a2f55a584
Salman Awan <salman.awan@medibank.com.au>
2017-01-06 15:57:09 +1100

    Test for place method

--- src/test/groovy/com/sawan/ioof/model/RobotTest.groovy
+++ src/test/groovy/com/sawan/ioof/model/RobotTest.groovy
@@ -1,39 +1,55 @@
 package com.sawan.ioof.model
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class RobotTest extends Specification {
 
     static Terrain terrain
     static Point point
     static Point pointInvalid
+    static Robot robot
 
     void setup() {
         terrain      = new TableTopTerrain(5, 5)
         point        = new Point(0, 0)
         pointInvalid = new Point(Point.VOID, Point.VOID)
+        robot        = new Robot(terrain)
     }
 
     def "Robot construction with terrain"() {
         when:
-        def robot = new Robot(terrain)
+        def r = robot
 
         then:
-        robot.curr == null
-        robot.facing == null
-        robot.terrain.width  == 5
-        robot.terrain.height == 5
+        r.curr == null
+        r.facing == null
+        r.terrain.width  == 5
+        r.terrain.height == 5
     }
 
+    @Unroll
     def "Robot is not positioned untill terrain is set and properly placed on it"() {
         when:
-        def robot = r
+        def r = robot
 
         then:
-        !robot.isPlaced()
+        !r.isPlaced()
 
         where:
         r << [new Robot(null), new Robot(terrain)]
     }
 
+    @Unroll
+    def "Robot throws when attempt to place incorrectly"() {
+        when:
+        r
+
+        then:
+        thrown(IllegalArgumentException.class)
+
+        where:
+        r << [robot.place(point, null),
+              robot.place(pointInvalid, Direction.EAST)]
+    }
 }
